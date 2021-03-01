85d4743647051d1c955d994c7baadca6f102b999
li_tl <763351670@qq.com>
2021-03-01 21:49:26 +0800

    修改提交

--- Front/blog-app/src/main.ts
+++ Front/blog-app/src/main.ts
@@ -6,13 +6,20 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
-import Highlight from './utils/Highlight'
 import less from 'less'
+import Highlight from './utils/highlight'
+import VueLazyload from 'vue-lazyload'
 
+Vue.prototype.$axios = axios  
 Vue.config.productionTip = false
 Vue.use(Highlight)
 Vue.use(less)
 Vue.use(ElementUI)
+/* eslint-disable no-new */
+Vue.use(VueLazyload, {
+  loading: require('./assets/img/loading.gif'),//加载中图片，一定要有，不然会一直重复加载占位图
+  error: require('./assets/img//error_img.jpg')  //加载失败图片
+});
 
 new Vue({
   router,
---
+++ Front/blog-app/src/utils/highlight.ts
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Hljs from 'highlight.js';
+import 'highlight.js/styles/monokai-sublime.css'
+
+interface Highlightjs {
+[x:string]:any
+}
+
+let Highlight:any = {};
+// let Highlight:any = {};
+
+Highlight.install = function (Vue:any,options:any) {
+
+    Vue.directive('highlightA',{
+        inserted:function(el:any){
+            let blocks = el.querySelectorAll('per code');
+            for (let i = 0; i < blocks.length; i++) {
+                const item = blocks[i]
+                Hljs.highlightBlock(item)
+            }
+        }
+    })
+
+    Vue.directive('highlightB',{
+        componentUpdated:function(el:any){
+            let blocks = el.querySelectorAll('per code');
+            for (let i = 0; i < blocks.length; i++) {
+                const item = blocks[i]
+                Hljs.highlightBlock(item)
+            }
+        }
+    })
+}
+
+export default Highlight
